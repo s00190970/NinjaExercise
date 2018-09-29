decd2d8e641f50a2d9524007b0bd777d0dc608e0
s00190970 <s00190970@mail.itsligo.ie>
2018-09-29 18:31:00 +0100

    S00190970 Exercise Finished

--- NinjaExcercise/NinjaConsole/Program.cs
+++ NinjaExcercise/NinjaConsole/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NinjaDomain.Classes;
+using NinjaDomain.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,69 @@ using System.Threading.Tasks;
 
 namespace NinjaConsole
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            // 1.	Return a ninja for a specified id
+            Console.WriteLine("{0} has id {1}", GetNinja(2).Name, 2);
+            Console.WriteLine();
+
+            // 2.	Return a list of ninjas and print them out
+            foreach (var ninjas in GetNinjaList())
+            {
+                Console.WriteLine(ninjas.Name);
+            }
+            Console.WriteLine();
+
+            // 3.	Return a list of the equipment owned by Ninja Paul
+            var paulsEquipment = GetNinjaEquipmentList(GetNinja(1));
+            foreach (var equipment in paulsEquipment)
+            {
+                Console.WriteLine(equipment.Name);
+            }
+            Console.WriteLine();
+
+            // 4.	Return a List of the Clans and their Ninja Members
+            var clanList = GetNinjaClanMemberLists();
+            foreach (var clan in clanList)
+            {
+                Console.WriteLine(clan.ClanName);
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        public static Ninja GetNinja(int id)
+        {
+            using(NinjaContext nc = new NinjaContext())
+            {
+                return nc.Ninjas.Where(i => i.Id == id).FirstOrDefault();
+            }
+        }
+
+        public static List<Ninja> GetNinjaList()
+        {
+            using(NinjaContext nc = new NinjaContext())
+            {
+                return nc.Ninjas.ToList();
+            }
+        }
+
+        public static List<NinjaEquipment> GetNinjaEquipmentList(Ninja ninja)
+        {
+            using(NinjaContext nc = new NinjaContext())
+            {
+                return nc.Equipment.Where(e => e.Ninja.Id == ninja.Id).ToList();
+            }
+        }
+        public static List<Clan> GetNinjaClanMemberLists()
+        {
+            using(NinjaContext nc = new NinjaContext())
+            {
+                return nc.Ninjas.Select(n => n.Clan).Distinct().ToList();
+            }
         }
     }
 }
--- NinjaExcercise/NinjaTests/UnitTest1.cs
+++ NinjaExcercise/NinjaTests/UnitTest1.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using NinjaDomain.Classes;
+using NinjaDomain.Classes;
 using System.Collections.Generic;
 using System.Linq;
 //using NUnit.Framework;
@@ -12,25 +12,25 @@ namespace NinjaTests
         [TestMethod]
         public void TestNinja()
         {
-            //Ninja n = NinjaConsole.Program.GetNinja(1);
-            //Assert.AreEqual(n.Name, "Ninja Paul");
+            Ninja n = NinjaConsole.Program.GetNinja(1);
+            Assert.AreEqual(n.Name, "Ninja Paul");
         }
 
         [TestMethod]
         public void TestNinjaCount()
         {
-            //int count = NinjaConsole.Program.GetNinjaList().Count; 
-            //Assert.AreEqual(count, 4);
+            int count = NinjaConsole.Program.GetNinjaList().Count;
+            Assert.AreEqual(count, 4);
         }
 
         [TestMethod]
         public void TestNinjaNames()
         {
-            //List<string> Ninjas = NinjaConsole.Program.GetNinjaList().Select(n => n.Name).ToList();
-            //CollectionAssert.Contains(Ninjas,"Ninja Paul","Ninja Paul not found");
-            //CollectionAssert.Contains(Ninjas, "Ninja Fred", "Ninja Fred not found");
-            //CollectionAssert.Contains(Ninjas, "Ninja Martha", "Ninja Martha not found");
-            //CollectionAssert.Contains(Ninjas, "Ninja Mary", "Ninja Mary not found");
+            List<string> Ninjas = NinjaConsole.Program.GetNinjaList().Select(n => n.Name).ToList();
+            CollectionAssert.Contains(Ninjas, "Ninja Paul", "Ninja Paul not found");
+            CollectionAssert.Contains(Ninjas, "Ninja Fred", "Ninja Fred not found");
+            CollectionAssert.Contains(Ninjas, "Ninja Martha", "Ninja Martha not found");
+            CollectionAssert.Contains(Ninjas, "Ninja Mary", "Ninja Mary not found");
         }
 
 
@@ -42,12 +42,12 @@ namespace NinjaTests
         public void TestNinjequipment()
         {
 
-            //List<NinjaEquipment> Equipment = NinjaConsole.Program.GetNinjaEquipmentList(new Ninja { Id = 1 }).ToList();
-            //List<string> EquipmentNames = new List<string>() { "Sweet thing", "Comona XL" };
-            //foreach (var item in EquipmentNames)
-            //{
-            //    CollectionAssert.Contains(Equipment.Select(e => e.Name).ToList(), item, item + " not found");
-            //}
+            List<NinjaEquipment> Equipment = NinjaConsole.Program.GetNinjaEquipmentList(new Ninja { Id = 1 }).ToList();
+            List<string> EquipmentNames = new List<string>() { "Sweet thing", "Comona XL" };
+            foreach (var item in EquipmentNames)
+            {
+                CollectionAssert.Contains(Equipment.Select(e => e.Name).ToList(), item, item + " not found");
+            }
         }
 
 
@@ -59,11 +59,11 @@ namespace NinjaTests
         [TestMethod]
         public void TestClanMembers()
         {
-            //List<Clan> clansWithMmmbers = NinjaConsole.Program.GetNinjaClanMemberLists();
-            //List<string> NinjasNames = clansWithMmmbers.First().Ninjas.Select(n => n.Name).ToList();
-            //Assert.AreEqual(clansWithMmmbers.First().ClanName, "Pauls Clan");
-            //CollectionAssert.Contains(NinjasNames, "Ninja Paul", "Ninja Paul not found");
-            //CollectionAssert.Contains(NinjasNames, "Ninja Fred", "Ninja Fred not found");
+            List<Clan> clansWithMmmbers = NinjaConsole.Program.GetNinjaClanMemberLists();
+            List<string> NinjasNames = clansWithMmmbers.First().Ninjas.Select(n => n.Name).ToList();
+            Assert.AreEqual(clansWithMmmbers.First().ClanName, "Pauls Clan");
+            CollectionAssert.Contains(NinjasNames, "Ninja Paul", "Ninja Paul not found");
+            CollectionAssert.Contains(NinjasNames, "Ninja Fred", "Ninja Fred not found");
         }
     }
 
